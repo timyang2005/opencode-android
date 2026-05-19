c0332cb6cea9d424ddedf0dac0e92853d8084311
timyang2005 <3072991924@qq.com>
2026-05-20 00:43:23 +0800

    Add project configuration files and CI/CD workflow

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
