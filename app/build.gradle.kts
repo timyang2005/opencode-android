536e1b911d1a3f84e6e7008043117235e00f8352
timyang2005 <3072991924@qq.com>
2026-05-20 05:10:44 +0800

    build: add freeCompilerArgs for experimental Compose APIs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
     }
     buildFeatures {
         compose = true
